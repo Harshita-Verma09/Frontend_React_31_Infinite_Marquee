f7c232e55b9cbbf53415093913bde467ca2628d9
Harshita Verma <soniharshita0908@gmail.com>
2025-03-31 01:37:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Marquee from "react-fast-marquee";
+
+function App() {
+ 
+  return (
+    <>
+      <Marquee gradient={false} speed={100}>
+        🚀 Welcome to my Infinite Marquee Text! 🎉 Enjoy smooth scrolling effects! ✨
+      </Marquee>
+    </>
+  )
+}
+
+export default App
